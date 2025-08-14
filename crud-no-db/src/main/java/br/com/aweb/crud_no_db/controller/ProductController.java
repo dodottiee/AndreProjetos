53485cff61018355a5780ad3c3b864baa72981e9
biels25 <ga.eduardo.correa@gmail.com>
2025-08-13 22:33:44 -0300

    gabriel

--- crud-no-db/src/main/java/br/com/aweb/crud_no_db/controller/ProductController.java
+++ crud-no-db/src/main/java/br/com/aweb/crud_no_db/controller/ProductController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.aweb.crud_no_db.dto.ProductDTO;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -38,6 +42,28 @@ public class ProductController {
        product.setId(nextId++);
        products.put(product.getId(), product);
        return product;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        if (products.remove(id) != null){
+            return  "Produto removido!";
+        }
+        return "Produto não encontrado";
+    }
+
+    @PutMapping("/{id}")
+    public String UpdateProducts (
+        @PathVariable Long id, 
+        @RequestBody ProductDTO product) {
+            
+        if (products.containsKey(id)){
+            products.get(id).setName(product.getName());
+            products.get(id).setPrice(product.getPrice());
+            return "Produto atualizado!";
+        }
+        
+        return "Produto não encontrado";
     }
     
 }
