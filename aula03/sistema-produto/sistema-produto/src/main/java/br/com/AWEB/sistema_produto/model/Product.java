cd9af1860c740c06884a4ac2e47e1f145528d393
cortezinho <gabrielcortez2802@gmail.com>
2025-08-25 19:17:44 -0300

    sistema-produto pronto

---
+++ aula03/sistema-produto/sistema-produto/src/main/java/br/com/AWEB/sistema_produto/model/Product.java
@@ -0,0 +1,80 @@
+package br.com.AWEB.sistema_produto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+public class Product {
+
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório!")
+    private String name;
+
+    @NotNull(message = "O preço é obrigatório!")
+    @PositiveOrZero(message = "Preço deve ser positivo ou zero!")
+    private Double price;
+
+    private String description;
+    
+
+    // Construtores
+    public Product() {}
+
+
+    public Product(Long id, String name, Double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Métodos Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
