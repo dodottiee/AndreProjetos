f5c9589997aa541f127c20b7180b7802df9b6381
biels25 <ga.eduardo.correa@gmail.com>
2025-08-31 20:24:14 -0300

    atividade andre

---
+++ sistema-aluno/sistema-aluno/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO aluno (nome, email, curso) VALUES 
+('João da Silva', 'joao.silva@email.com', 'Ciência da Computação'),
+('Maria Oliveira', 'maria.oliveira@email.com', 'Engenharia de Produção'),
+('Pedro Santos', 'pedro.santos@email.com', 'Administração'),
+('Ana Souza', 'ana.souza@email.com', 'Ciência da Computação'),
+('Carlos Pereira', 'carlos.pereira@email.com', 'Engenharia Elétrica');
