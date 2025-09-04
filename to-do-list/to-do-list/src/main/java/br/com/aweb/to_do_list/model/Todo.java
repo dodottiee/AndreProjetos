ade53fa6b8be8963b9edddb230be30db17eb4f2b
MykyBalb <mykaelbalbino06@gmail.com>
2025-09-03 22:43:23 -0300

    Aula03/09

--- to-do-list/to-do-list/src/main/java/br/com/aweb/to_do_list/model/Todo.java
+++ to-do-list/to-do-list/src/main/java/br/com/aweb/to_do_list/model/Todo.java
@@ -2,12 +2,20 @@ package br.com.aweb.to_do_list.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,12 +29,17 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 3, max = 100)
+    @NotBlank
     @Column(nullable = false, length = 100)
     private String title;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    @NotNull
+    @FutureOrPresent
+    @DateTimeFormat(iso = ISO.DATE)
     @Column(nullable = false)
     private LocalDate deadline;
 
